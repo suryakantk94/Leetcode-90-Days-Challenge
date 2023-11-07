0d68a0866b972d9e77fb02597466f142d15ebef2
Suryakant Kashyap <59741292+suryakantk94@users.noreply.github.com>
2023-11-06 21:11:52 -0800

    Recursion

---
+++ Recursion - permute string with changing cases/README.md
@@ -0,0 +1,11 @@
+## Problem
+
+Print all permutations of a string keeping the sequence but changing cases.
+
+Examples:
+
+Input : ab
+Output : AB Ab ab aB
+
+Input : ABC
+Output : abc Abc aBc ABc abC AbC aBC ABC
---
+++ Recursion - permute string with changing cases/permute_changing_cases.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void helper(string &s, int idx, string slate, vector<string> &result)
+{
+    if (s.size() == idx)
+    {
+        result.push_back(slate);
+        return;
+    }
+
+    // Inclusion
+    //  cout<<"UP"<<up;
+    helper(s, idx + 1, slate + s[idx], result);
+
+    // Exclusion
+    char up = toupper(s[idx]);
+    helper(s, idx + 1, slate + up, result);
+}
+
+int main()
+{
+    string s = "ab";
+    int idx = 0;
+    string slate;
+    vector<string> result;
+
+    helper(s, idx, slate, result);
+
+    // Print the results
+    for (const string &str : result)
+    {
+        cout << str << endl;
+    }
+
+    return 0;
+}
